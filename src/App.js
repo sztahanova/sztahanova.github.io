bab0554e401b6e8aa9bb59966c16a38023a934bd
sztahanova <sztahanova@github.com>
2021-02-05 19:34:23 +0100

    Initial CRA commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './green-computer-code.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="green-computer-code" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
